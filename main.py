add1e806f477236f267b61fcf0f465cfc21f9b80
Mark Peters <mapeters101@earthlink.net>
2024-09-14 19:01:34 -0400

    Add header, total price, company and logo

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+    pdf.ln(5)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
@@ -40,6 +41,26 @@ for filepath in filepaths:
         pdf.cell(w=33, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=33, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=25, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=33, h=8, txt="", border=1)
+    pdf.cell(w=33, h=8, txt="", border=1)
+    pdf.cell(w=33, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.ln(5)
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=33, h=8, txt=f"The total price is {total_sum}.", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=31, h=8, txt=f"PythonHow")
+    pdf.image("images/Pythonhow.png", w=7)
 
 
     pdf.output(f"PDFs/{filename}.pdf")
